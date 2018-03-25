42343b86dfab724c06207692c5d751d36011665e
merav2110 <32419243+merav2110@users.noreply.github.com>
2018-03-25 04:24:13 +0300

    Merav-04 changing the localhost IP

--- src/main/java/com/merav/testing/MySQLConnectorImpl.java
+++ src/main/java/com/merav/testing/MySQLConnectorImpl.java
@@ -21,7 +21,7 @@ public class MySQLConnectorImpl implements  MySQLConnector{
         System.out.println("MySQL JDBC Driver Registered!");
         try {
             connection = DriverManager
-                    .getConnection("jdbc:mysql://127.0.0.1:3306/results", "root", "skyAreBlue123");
+                    .getConnection("jdbc:mysql://0.0.0.0:3306/results", "root", "skyAreBlue123");
 
         } catch (SQLException e) {
             System.out.println("Connection Failed! Check output console");
